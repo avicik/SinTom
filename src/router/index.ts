d8ac964962654cefd58534f5b90501f50f4ff22b
avicik <1902885899@qq.com>
2024-10-27 18:44:07 +0800

    123123

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,34 +2,34 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 import HomePage from '../views/HomePage.vue'
 
 const router = createRouter({
-  history: createWebHashHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory('/SinTom/'),
   routes: [
     {
       path: '/',
       name: 'home',
-      component: HomePage
+      component: HomePage,
     },
     {
       path: '/chart',
       name: 'chart',
-      component: () => import('../views/ChartPage.vue')
+      component: () => import('../views/ChartPage.vue'),
     },
     {
       path: '/smile',
       name: 'smile',
-      component: () => import('../views/SmilePage.vue')
+      component: () => import('../views/SmilePage.vue'),
     },
     {
       path: '/bell',
       name: 'bell',
-      component: () => import('../views/BellPage.vue')
+      component: () => import('../views/BellPage.vue'),
     },
     {
       path: '/search',
       name: 'search',
-      component: () => import('../views/SearchPage.vue')
-    }
-  ]
+      component: () => import('../views/SearchPage.vue'),
+    },
+  ],
 })
 
 export default router
