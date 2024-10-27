3b39004fb3caa1333aa76d6866f377e5ed2b2c93
avicik <1902885899@qq.com>
2024-10-27 18:27:28 +0800

    适配GitHub Pages

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,7 @@
 import { fileURLToPath } from 'node:url'
 import { mergeConfig, defineConfig, configDefaults } from 'vitest/config'
 import viteConfig from './vite.config'
+import vue from '@vitejs/plugin-vue'
 
 export default mergeConfig(
   viteConfig,
@@ -9,6 +10,8 @@ export default mergeConfig(
       environment: 'jsdom',
       exclude: [...configDefaults.exclude, 'e2e/**'],
       root: fileURLToPath(new URL('./', import.meta.url))
-    }
+    },
+    plugins: [vue()],
+    base: '/https://github.com/avicik/SinTom/', // 替换为你的 GitHub 仓库名
   })
 )
